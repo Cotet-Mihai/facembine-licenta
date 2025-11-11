6e0f1372c45cc83abf8782356380f783f4e14d51
Cotet Mihai <cotet.mihaita.cornel@gmail.com>
2025-11-11 22:51:17 +0200

    completed design for first section in HomePage (desktop + mobile)

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import {Input} from "@/components/ui/input";
 import {Button} from "@/components/ui/button";
 import {Search} from "lucide-react";
+import Image from 'next/image'
 
 export default function Home() {
   return (
       <div>
-          <div className={'h-[calc(100vh-3rem)] flex justify-center items-center'}>
+          <div className={'h-[calc(100vh-3rem)] flex justify-center items-center flex-col sm:flex-row'}>
               <div>
                   <div className={'w-full max-w-4xl px-4 flex flex-col justify-center items-center sm:items-start'}>
                       <h3 className={'text-3xl text-green-600 sm:text-4xl'}>Găsește evenimente,</h3>
@@ -20,10 +21,15 @@ export default function Home() {
                       </Button>
                   </div>
               </div>
-
+              <div className={'hidden sm:block'}>
+                  <Image src={'/webp/home_image.webp'} width={500} height={500} alt={'Home Image'}/>
+              </div>
+              <div className={'block sm:hidden'}>
+                  <Image src={'/webp/home_image.webp'} width={300} height={300} alt={'Home Image'}/>
+              </div>
           </div>
-          <div className={'h-[calc(100vh-3rem)] bg-blue-500'}>
-
+          <div className={'h-[calc(100vh-3rem)] bg-green-500 flex justify-center'}>
+                <h4 className={'text-5xl font-semibold mt-6 uppercase text-green-900'}>Sponsori în care avem încredere!</h4>
           </div>
       </div>
 
